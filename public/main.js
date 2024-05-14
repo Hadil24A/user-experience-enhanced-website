fa7d8ac6710641ca3758a5e6cc59771a333bc1d2
Hadil24A <hadil.ahmed@hva.nl>
2024-05-14 10:52:00 +0200

    prettier

--- public/main.js
+++ public/main.js
@@ -1,60 +1,56 @@
-if (document.startViewTransition) { 
-    document.startViewTransition(function() {
-        document.querySelector('.heart').innerHTML = responseHTML
-    });
-} else { 
-    document.querySelector('.heart').innerHTML = responseHTML
-}
+// if (document.startViewTransition) {
+//     document.startViewTransition(function() {
+//         document.querySelector('.heart').innerHTML = responseHTML
+//     });
+// } else {
+//     document.querySelector('.heart').innerHTML = responseHTML
+// }
 
 let navigatie = document.querySelector(".navigatie");
 let menu = document.querySelector(".home-menu");
 
 if (navigatie) {
-    navigatie.addEventListener("click", () => {
-      
-      navigatie.classList.toggle("active");
-      menu.classList.toggle("home-menu-active");
-    });
+  navigatie.addEventListener("click", () => {
+    navigatie.classList.toggle("active");
+    menu.classList.toggle("home-menu-active");
+  });
 }
 
 if (gsap) {
-    gsap.to('.muis', {
-        duration:2.5,
-        ease: "bounce.in",
-        y: -225,
-        scale: 0.8,
-    });
+  gsap.to(".muis", {
+    duration: 2.5,
+    ease: "bounce.in",
+    y: -225,
+    scale: 0.8,
+  });
 }
 
-let form = this 
-form.classList.add('isLoading')
+let form = this;
+form.classList.add("isLoading");
 
-
-let imageUpload = document.getElementById('image-upload');
+let imageUpload = document.getElementById("image-upload");
 if (imageUpload) {
-
-    let previewImage = () => {
+  let previewImage = () => {
     let files = imageUpload.files;
 
-        if (files && files.length > 0) {
+    if (files && files.length > 0) {
+      let fileReader = new FileReader();
+      let preview = document.getElementById("addImg");
+      let label = document.querySelector('label[for="image-upload"]');
 
-            let fileReader = new FileReader();
-            let preview = document.getElementById('addImg');
-            let label = document.querySelector('label[for="image-upload"]'); 
-
-            fileReader.onload = function (event) {
-                preview.style.display = 'block';
-                preview.setAttribute('src', event.target.result);
-                label.style.display = 'none'; 
-            }
-            fileReader.readAsDataURL(files[0]);
-        }
+      fileReader.onload = function (event) {
+        preview.style.display = "block";
+        preview.setAttribute("src", event.target.result);
+        label.style.display = "none";
+      };
+      fileReader.readAsDataURL(files[0]);
     }
-    imageUpload.addEventListener("change", previewImage);
+  };
+  imageUpload.addEventListener("change", previewImage);
 }
 
-let newPlaylist = []
+let newPlaylist = [];
 if (newPlaylist) {
-newPlaylist.push()
-console.log(newPlaylist)
+  newPlaylist.push();
+  console.log(newPlaylist);
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import fetchJson from "./helpers/fetch-json.js";
 
 let app = express();
 
-let newPlaylist = []  // Niewe playlist aanmaken
+let newPlaylist = []; // Niewe playlist aanmaken
 
 let favourites = {};
 
@@ -19,91 +19,98 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 // 2. ROUTES
-   
-app.get("/", function (request, response) {      
-    response.render("homepage", {});          
-  });  
 
-  app.get("/testing", function (request, response) {      
-    response.send("testing");         
-  });  
-      
-  app.get("/lessons", function (request, response) {
-    Promise.all([
-        fetchJson('https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width'),
-        fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-        fetchJson('https://fdnd-agency.directus.app/items/tm_playlist?fields=*,image.id,image.height,image.width'),
-        fetchJson('https://fdnd-agency.directus.app/items/tm_audio')
-    ]).then(([storyData, languageData, playlistData, audioData]) => {
-        let likedPlaylists = playlistData.data.filter(playlist => favourites[playlist.id]);
-        let playlistIds = playlistData.data.map(playlist => playlist.id);
-
-        response.render('lessons', {
-            stories: storyData.data,
-            language: languageData.data,
-            playlist: playlistData.data,
-            audio: audioData.data,
-            favourites: favourites,
-            likedPlaylists: likedPlaylists, 
-            newPlaylists: newPlaylist,
-            playlistIds: playlistIds
-        });
-    });
+app.get("/", function (request, response) {
+  response.render("homepage", {});
 });
 
-app.post('/like-or-unlike/:playlistId', function(request, response) {
+app.get("/testing", function (request, response) {
+  response.send("testing");
+});
+
+app.get("/lessons", function (request, response) {
+  Promise.all([
+    fetchJson(
+      "https://fdnd-agency.directus.app/items/tm_story?fields=*,image.id,image.height,image.width"
+    ),
+    fetchJson("https://fdnd-agency.directus.app/items/tm_language"),
+    fetchJson(
+      "https://fdnd-agency.directus.app/items/tm_playlist?fields=*,image.id,image.height,image.width"
+    ),
+    fetchJson("https://fdnd-agency.directus.app/items/tm_audio"),
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
+    let likedPlaylists = playlistData.data.filter(
+      (playlist) => favourites[playlist.id]
+    );
+    let playlistIds = playlistData.data.map((playlist) => playlist.id);
+
+    response.render("lessons", {
+      stories: storyData.data,
+      language: languageData.data,
+      playlist: playlistData.data,
+      audio: audioData.data,
+      favourites: favourites,
+      likedPlaylists: likedPlaylists,
+      newPlaylists: newPlaylist,
+      playlistIds: playlistIds,
+    });
+  });
+});
+
+app.post("/like-or-unlike/:playlistId", function (request, response) {
   let playlistId = Number(request.params.playlistId);
   let action = request.body.action;
   console.log(action, playlistId);
 
-  if (action === 'like') {
-      favourites[playlistId] = true;
-  } else if (action === 'unlike') {
-      delete favourites[playlistId];
+  if (action === "like") {
+    favourites[playlistId] = true;
+  } else if (action === "unlike") {
+    delete favourites[playlistId];
   }
 
-  response.redirect(303, '/lessons');
+  response.redirect(303, "/lessons");
 });
 
-  // if (request.body.enhanced) {
-  //     response.render('playlist');
-  // } else {
-  //     response.redirect(303, '/lessons');
-  // }
+// if (request.body.enhanced) {
+//     response.render('playlist');
+// } else {
+//     response.redirect(303, '/lessons');
+// }
 
-  app.get("/allStories", function (request, response) {      
-    Promise.all([
-      fetchJson('https://fdnd-agency.directus.app/items/tm_story'),
-      fetchJson('https://fdnd-agency.directus.app/items/tm_language'),
-      fetchJson('https://fdnd-agency.directus.app/items/tm_audio')]).then(([storyData, languageData, playlistData, audioData]) => {    
-      response.render('allStories', {
-        stories: storyData.data, 
-        language: languageData.data,
-        playlist: playlistData.data,
-      });
+app.get("/allStories", function (request, response) {
+  Promise.all([
+    fetchJson("https://fdnd-agency.directus.app/items/tm_story"),
+    fetchJson("https://fdnd-agency.directus.app/items/tm_language"),
+    fetchJson("https://fdnd-agency.directus.app/items/tm_audio"),
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
+    response.render("allStories", {
+      stories: storyData.data,
+      language: languageData.data,
+      playlist: playlistData.data,
     });
   });
+});
 
-  app.get("/new-playlist", function (request, response) {
-    response.render("new-playlist", {});
-  })
+app.get("/new-playlist", function (request, response) {
+  response.render("new-playlist", {});
+});
 
-  app.post("/new-playlist", function (request, response) { 
-    newPlaylist.push(request.body(newPlaylists))
-    console.log(newPlaylist);
-    response.redirect(303, '/lessons/');
-  })
-  
-  app.get("/statistics", function (request, response) {      
-    response.send("statistics");         
-  });
-  
-  app.get("/profile", function (request, response) {      
-    response.send("profile");         
-  });
-  
+app.post("/new-playlist", function (request, response) {
+  newPlaylist.push(request.body(newPlaylists));
+  console.log(newPlaylist);
+  response.redirect(303, "/lessons/");
+});
+
+app.get("/statistics", function (request, response) {
+  response.send("statistics");
+});
+
+app.get("/profile", function (request, response) {
+  response.send("profile");
+});
+
 // 3. START DE WEBSERVER
- 
+
 app.set("port", process.env.PORT || 8000);
 app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
